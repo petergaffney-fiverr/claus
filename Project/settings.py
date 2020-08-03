163a4fe7f47d2a90bbe7df31e596268156094279
A26-Lab <a26.lab.syl@gmail.com>
2020-08-03 15:56:17 +0600

    modify

--- Project/settings.py
+++ Project/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = '/statics/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "statics")]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, "media")
